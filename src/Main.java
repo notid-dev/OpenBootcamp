439f079c3e5c7f7189fadd2e2538d44372b90d53
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2022-07-15 21:16:06 -0500

    primer uso

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void suma(String[] args) {
+        int resultado;
+        resultado = suma(50,30,20);
+        System.out.println(resultado);
+    }
+
+    public static int suma (int a, int b, int c){
+
+        return a + b + c;
+    }
+
+}
+
+
+
